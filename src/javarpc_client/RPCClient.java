408e312d23a39a2237d8c92e27408f90f4c9404b
santensuru <djuned.ong@gmail.com>
2015-03-19 00:49:47 +0700

    Init

---
+++ src/javarpc_client/RPCClient.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javarpc_client;
+import com.rabbitmq.client.AMQP.BasicProperties;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.QueueingConsumer;
+
+/**
+ *
+ * @author user
+ */
+
+public class RPCClient{
+    private Connection connection;
+    private Channel channel;
+    private String requestQueueName = "rpc_queue";
+    private String replyQueueName;
+    private QueueingConsumer consumer;
+
+    public RPCClient() throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("10.151.12.201");
+        connection = (Connection) factory.newConnection();
+        channel = connection.createChannel();
+
+        replyQueueName = channel.queueDeclare().getQueue(); 
+        consumer = new QueueingConsumer(channel);
+        channel.basicConsume(replyQueueName, true, consumer);
+    }
+
+    public String call(String message) throws Exception {     
+        String response = null;
+        String corrId = java.util.UUID.randomUUID().toString();
+
+        BasicProperties props = new BasicProperties
+                                    .Builder()
+                                    .correlationId(corrId)
+                                    .replyTo(replyQueueName)
+                                    .build();
+
+        channel.basicPublish("", requestQueueName, props, message.getBytes());
+
+        while (true) {
+            QueueingConsumer.Delivery delivery = consumer.nextDelivery();
+            if (delivery.getProperties().getCorrelationId().equals(corrId)) {
+                response = new String(delivery.getBody());
+                break;
+            }
+        }
+
+        return response; 
+    }
+
+    public void close() throws Exception {
+        connection.close();
+    }
+}
